95e0a73b46866f202a76b47db66f09513fdd9c78
adrianRD <adrian.33ice@hotmail.com>
2023-04-10 18:35:55 -0600

    Add elixir project

---
+++ bpt-elixir-01/lib/bpt_elixir01.ex
@@ -0,0 +1,18 @@
+defmodule BptElixir01 do
+  @moduledoc """
+  Documentation for `BptElixir01`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BptElixir01.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ bpt-elixir-01/test/bpt_elixir01_test.exs
@@ -0,0 +1,8 @@
+defmodule BptElixir01Test do
+  use ExUnit.Case
+  doctest BptElixir01
+
+  test "greets the world" do
+    assert BptElixir01.hello() == :world
+  end
+end
